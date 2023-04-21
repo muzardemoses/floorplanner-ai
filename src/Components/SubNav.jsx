3bee9b989612ef7dd18f66c3a54b5176c5e65465
muzardemoses <muzardemoses@gmail.com>
2023-04-21 01:16:28 +0100

    modify dashboard

--- src/Components/SubNav.jsx
+++ src/Components/SubNav.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import logo from "../Images/logo.png";
+import { Link } from "react-router-dom";
 
 export const SubNav = () => {
   return (
-    <div>
-      <h1>SubNav</h1>
+    <div className=" bg-white w-60 flex flex-col justify-between items-center shadow py-10">
+      <img src={logo} alt="logo" className=" h-8 " />
+      <Link to="/login">
+        <button className="bg-teal-500 text-white h-10 px-14 rounded-md">
+          Log Out
+        </button>
+      </Link>
     </div>
   );
 };
--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { SubNav } from "../Components/SubNav";
 
 export const DashboardLayout = ({ children }) => {
   return (
-    <div>
-      <h1>Dashboard Layout</h1>
+    <div
+      style={{ backgroundColor: "rgb(246, 248, 255)" }}
+      className=" min-h-screen flex"
+    >
+      <SubNav />
       {children}
     </div>
   );
